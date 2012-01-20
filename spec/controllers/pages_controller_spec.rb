cf68ff36f205aadfde3ef75479db80e82e4adc84
lordkrishnasonamity <prashantnair.amity@gmail.com>
2012-01-20 21:13:38 +0530

    titles put

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+	render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+	    get 'home'
+	    response.should have_selector("title", :content => "ruby on rails tutorial sample app | home")
+  end
   end
 
   describe "GET 'contact'" do
@@ -14,6 +20,25 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    
+    it "should have the right title" do
+	    get 'contact'
+	    response.should have_selector("title", :content => "ruby on rails tutorial sample app | contact")
+  end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+
+    
+    it "should have the right title" do
+	    get 'about'
+	    response.should have_selector("title", :content => "ruby on rails tutorial sample app | about")
+  end
   end
 
 end
